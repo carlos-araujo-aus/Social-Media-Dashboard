8a5e27781cdaed3d18907a341127c16a98d535fa
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-10-24 08:40:57 +0800

    fourth commit

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-import express from 'express';
-import jwt from J
-import { jwt } from 'jsonwebtoken';
+import empress from 'express';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import dotenv from 'dotenv'
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT;
-
-app.use(express.json());
-
-const secretKey = process.env.SECRET_KEY;
-const users = [];
-
-function au
