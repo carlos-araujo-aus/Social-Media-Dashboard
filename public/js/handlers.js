867cf58f734c2c08dd266a85574b37d7fc7b6158
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-11-05 08:56:43 +0800

    frontend logic finalized

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -2,9 +2,13 @@ const registerForm = document.getElementById("registerForm");
 const loginForm = document.getElementById("loginForm");
 const findPostsForm = document.getElementById("findPostsForm")
 const postForm = document.getElementById("postForm")
+const postTextBox = document.getElementById("postTextBox")
+const originalPostContainer = document.getElementById("original-post-container")
+const postTextBoxEdit = document.getElementById("postTextBoxEdit")
 
 const postList = document.getElementById("posts-list");
 const isLoading = document.getElementById("posts-loading");
+const charCount = document.getElementById("charCount")
 
 const myPostsBtn = document.getElementById("mypostsbtn");
 const allPostsBtn = document.getElementById("allPosts");
@@ -12,6 +16,9 @@ const logoutBtn = document.getElementById("logout")
 const listAllUsersBtn = document.getElementById("listAllUsers")
 const userByUsernameBtn = document.getElementById("userByUsername")
 const sendPostBtn = document.getElementById("sendPost")
+const editPostBtn = document.getElementById("editPostBtn")
+const deletePostBtn = document.getElementById("deletePostBtn")
+const postNewPostBtn = document.getElementById("postNewPostBtn")
 
 const handleRegisterSubmit = async (e) => {
     e.preventDefault();
@@ -83,12 +90,17 @@ const handleMyPosts = async (e) => {
         if (posts.length > 0) {
             postList.innerHTML = posts.map(i => 
                 `
-                <p><strong>Date: </strong>${new Date(i.date).toLocaleDateString()}</p>
-                <p><strong>Text: </strong>${i.text}</p>
-                <p><strong>Username: </strong>${username}</p>
-                </br>
+                </br></br></br>
+                <div post-container-id="${i.id}">
+                    <p><strong>Date: </strong>${new Date(i.date).toLocaleDateString()}</p>
+                    <p><strong>Text: </strong>${i.text}</p>
+                    <p><strong>Username: </strong>${username}</p>
+                    <button class="btn-primary" onclick="handleEditPost('${i.id}', '${i.text}', '${username}', '${new Date(i.date).toLocaleDateString()}')">Edit Post</button>
+                    <button class="btn-primary" onclick="handleDeletePost('${i.id}')">Delete Post</button>
+                </div>
                 `
             ).join("");
+
         } else {
             postList.innerHTML = `
                 <p><strong>You do not hjave any post yet</p>
@@ -96,6 +108,86 @@ const handleMyPosts = async (e) => {
         }
     } catch (error) {
         console.error("Error: " , error);
+    }
+}
+
+
+const handleEditPost = async (postId, postText, username, postDate) => {
+    try {
+        sessionStorage.setItem("editPostData", JSON.stringify({
+            id: postId,
+            text: postText,
+            username: username,
+            date: postDate
+        }));
+        window.location.href = "/post.html"
+    } catch (error) {
+        console.error("Error: ", error);
+        postList.innerHTML = `<p style="color: red; text-align: center;">❌ ${error.message}</p>`;
+    }
+}
+
+const fillPost =  () => {
+    try {
+        const postData = JSON.parse(sessionStorage.getItem("editPostData"))
+        if (!postData) {
+            throw new Error(errorData.message || "There was an error");
+        }
+        originalPostContainer.innerHTML = `
+            <p><strong>Date: </strong>${new Date(postData.date).toLocaleDateString()}</p>
+            <p><strong>Text: </strong>${postData.text}</p>
+            <p><strong>Username: </strong>${postData.username}</p>
+        ` 
+        postTextBoxEdit.value = postData.text
+    } catch (error) {
+        console.error("Error: ", error);
+        originalPostContainer.innerHTML = `<p style="color: red; text-align: center;">❌ ${error.message}</p>`;
+    }
+}
+const handleUpdatePost = async (e) => {
+    e.preventDefault()
+    try {
+        const textToUpdate = postTextBoxEdit.value
+        const postData = JSON.parse(sessionStorage.getItem("editPostData"))
+        const response = await fetch (`/updatepost/${postData.id}`, {
+            method: "PUT",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({newText: textToUpdate})
+        })
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(errorData.message || "There was an error sending your post");
+        } else {
+            originalPostContainer.innerHTML = `
+            <p>✅ your post has been updated!</P>
+            <p><strong>New Text: </strong>${textToUpdate}</p></br>
+            `;
+            postTextBoxEdit.value = "";
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+        originalPostContainer.innerHTML = `<p style="color: red; text-align: center;">❌ ${error.message}</p>`;
+    }
+}
+const handleDeletePost = async (e) => {
+    try {
+        if (!confirm("Are you sure you want to delete this post?")) {
+            return;
+        }
+        const response = await fetch (`/deletepost/${e}`, {
+            method: "DELETE",
+            headers: { 'Content-Type': 'application/json' },
+        })
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(errorData.message || "There was an error deleting the post");
+        } else {
+            postList.innerHTML = `<p>✅ your post has been deteletd!</P>`
+        }
+
+    } catch (error) {
+        console.error("Error: ", error);
+        postList.innerHTML = `<p style="color: red; text-align: center;">❌ ${error.message}</p>`;
     }
 }
 
@@ -241,6 +333,26 @@ const handleSendPost = async (e) => {
     }
 }
 
+const handleCharCount = async (e) => {
+    e.preventDefault
+    try {
+        const textContent = postTextBox.value
+        const textLength = textContent.length + 1
+        if (textLength > 0) {
+            charCount.innerHTML = `<p text-align: center;">${textLength}/500</p>`
+        } 
+        if (textLength === 0) {
+            charCount.innerHTML = `<p text-align: center;">0/500</p>`
+        }
+        if (textLength >= 500) {
+            charCount.innerHTML = `<p style="color: red; text-align: center;">500/500</p>`
+        }
+        
+    } catch (error) {
+        console.error("Error: " , error);
+    }
+}
+
 
 const handleLogout = async (e) => {
     e.preventDefault();
@@ -275,8 +387,15 @@ myPostsBtn?.addEventListener("click", (e) => {
     handleIsLoading(e); 
 })
 allPostsBtn?.addEventListener("click", handleAllPosts);
-logoutBtn?.addEventListener("click", handleLogout)
-listAllUsersBtn?.addEventListener("click", handleListAllUsers)
-userByUsernameBtn?.addEventListener("click", handleShowFindUserForm)
+logoutBtn?.addEventListener("click", handleLogout);
+listAllUsersBtn?.addEventListener("click", handleListAllUsers);
+userByUsernameBtn?.addEventListener("click", handleShowFindUserForm);
 findPostsForm?.addEventListener("submit", handleUserByUsername)
-postForm?.addEventListener("submit", handleSendPost)
+postForm?.addEventListener("submit", handleSendPost);
+postTextBox?.addEventListener("keydown", handleCharCount);
+postNewPostBtn?.addEventListener("click", handleUpdatePost);
+document.addEventListener("DOMContentLoaded", () => {
+    if (originalPostContainer) {
+        fillPost()
+    }
+})
